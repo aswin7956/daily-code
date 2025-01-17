df300005503d95956027b66d9fb7f56e85945f7a
rbai8913 <rbai8913@gmail.com>
2025-01-17 17:04:39 +0000

    First pgm

---
+++ code1.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def threeSum(self, nums):  
+        a = []  #initialize empty list
+        nums.sort()  
+        
+        if nums[0] > 0:  
+            return a  #early returning
+        
+        for i in range(len(nums)):  
+            if i > 0 and nums[i] == nums[i - 1]:  #skip duplicate values 
+                continue
+
+            j = i + 1  
+            k = len(nums) - 1  
+
+            while j < k:  # Use two pointers to find pairs that sum to the negative of nums[i]
+                s = nums[i] + nums[j] + nums[k]  #calculate the sum of the current triplet
+                if s == 0: 
+                    a.append([nums[i], nums[j], nums[k]])  #add the triplet to 'a' id the sum is found
+                    j += 1  
+                    while nums[j] == nums[j - 1] and j < k:  #skip duplicates 
+                        j += 1
+                elif s > 0:  # the sum is greater so decrement k to reduce s
+                    k -= 1
+                else: 
+                    j += 1
+
+        return a
+
+a=Solution()
+print(a.threeSum([-1,0,1,2,-1,-4])) # For local testing
