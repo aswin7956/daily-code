6bb4b393e3359c09176310458cdc1d6988c07b03
aswin7956 <ashwin202507@gmail.com>
2025-01-18 14:10:26 +0000

    Pgm 6

---
+++ code6.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums, target):
+        s = {}  #map to store index with val
+        for i in range(len(nums)):  
+            comp = target - nums[i]  #find the complement of target
+            if comp in s.keys():  
+                return [s[comp], i] # return indices when a match is found
+            s[nums[i]] = i  
