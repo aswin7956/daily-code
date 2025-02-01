492f1ac3025726e75b4ef87f4627cf3deeb23063
aswin7956 <ashwin202507@gmail.com>
2025-02-01 14:58:37 +0000

    feb1

--- code26.java
+++ code26.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class SimpleCalculatorDoWhile {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("This is a simple Calculator");
         int num1, num2;
         char operator;
-        boolean loop;  // initialized default value false unlike previous program
-        do {          // executes once always
+        boolean loop = false;     // to store user choice initially tofalse
+        do {                      // runs atleast once 
             System.out.println("Num1 : ");
             num1 = scanner.nextInt();         //   get operands
             System.out.println("Num2 : ");
             num2 = scanner.nextInt();
             System.out.println("operator : ");  // get operaor
             operator = scanner.next().charAt(0);
-            switch (operator) {         // switch case for operatoions
+            switch (operator) {            // loop until user prompts to stop program execution
                 case '+':
                     System.out.println(num1 + num2);
                     break;
@@ -24,20 +25,24 @@ public class SimpleCalculatorDoWhile {
                     System.out.println(num1 * num2);
                     break;
                 case '/':
-                    if (num2 != 0) {      // handling error thrown
+                    if (num2 != 0) {             // division by zero causes error
                         System.out.println(num1 / num2);
                     } else {
                         System.out.println("division by zero");
                     }
                     break;
-                default:                             // default to handle other inputs
-                    System.out.println("Invalid operator.");
+                default:
+                    System.out.println("invalid operator");
                     break;
             }
-            System.out.println("Continue ? yes : no ");
-            String res = scanner.next();      // prompt user to ask for continuation
+            System.out.println("Continue ? yes : no "); // prompt user to ask for continuation
+            String res = scanner.next();  
+            if (res.equalsIgnoreCase("yes")) {
+                loop = true;
+            }    
             if (res.equalsIgnoreCase("no")) {
                 loop = false;
             }
+        }while (loop);
     }
 }
