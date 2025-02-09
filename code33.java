3181b9ebbdda751f8f914546f2087c45e779c511
aswin7956 <ashwin202507@gmail.com>
2025-02-09 16:43:38 +0000

    feb9

--- code33.java
+++ code33.java
@@ -1,17 +1,17 @@
 public class Main {
     public static int hillWeight(int N, int W, int D) {
-        int tot = 0;
-        int x = W;
+        int tot = 0; // total weight
+        int x = W; // initial weight in each row
         for (int i = 0; i < N; i++) {
-            for (int s = 0; s < N - i - 1; s++) {
+            for (int s = 0; s < N - i - 1; s++) {  // whitespace logic
                 System.out.print("  ");
             }
-            for (int j = 0; j <= i; j++) {
-                tot += x;
+            for (int j = 0; j <= i; j++) { // hill logic
+                tot += x; // add weight to toal iterating the row
                 System.out.print(x + " ");
             }
             System.out.println(); 
-            x += D;
+            x += D; // increment weight by D
         }
         return tot;
     }
