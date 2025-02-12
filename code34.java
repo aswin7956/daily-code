7bdd459f54a449503a4ddb6457150ae4c0f31b0a
aswin7956 <ashwin202507@gmail.com>
2025-02-12 15:42:30 +0000

    12feb

---
+++ code34.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static String lcs(String s1, String s2) {
+        String LCS = ""; // emoty string to store final res
+        for (int i = 0; i < s1.length(); i++) { // iterate over first str
+            for (int j = i + 1; j <= s1.length(); j++) { // iterate i+1 to end
+                String substr = s1.substring(i, j); // get all possible substrings
+                if (s2.contains(substr) && substr.length() > LCS.length()) { // if substr found and is greater than current max length
+                    LCS = substr; // update
+                }
+            }
+        }
+        return LCS;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcdef";
+        String s2 = "zabcdf";
+        String result = lcs(s1, s2);
+        System.out.println("Longest Common Substring: " + result);
+    }
+}
